b4e473e265dfd6952a1aac0dc487300140e424c6
bisiman2 <bcadesanoye@gmail.com>
2022-12-02 10:56:04 -0800

    Updated DBConnect.java file

---
+++ DBConnect.java
@@ -0,0 +1,13 @@
+mkdir git-code
+#cd git-code
+#git init : Create a local Git empty repository.
+Initialized empty Git repository in /Users/SimonLegah/git/git-code/.git/
+#git status : Gives the status of your untracked files.
+#touch DBConnect.java
+#git status
+#vim DBConnect.java
+#git add DBConnect.java: Add the files(here DBConnect.java) into your staging area. #git status
+On branch master Initial
+commit
+Changes to be committed:
+(use "git rm --cached <file>..." to unstage) new file
